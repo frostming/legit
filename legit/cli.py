d4dad39d9d97aac570cd7c766842ec6c0aa52832
Kenneth Reitz <me@kennethreitz.com>
2011-06-25 12:37:18 -0400

    commands list

--- legit/cli.py
+++ legit/cli.py
@@ -9,23 +9,30 @@ This module povides the CLI interface to legit.
 
 import sys
 
-import clint
+from clint import args
+from clint.eng import join as eng_join
 from clint.textui import colored, indent, puts
 
 from .core import __version__
 
 
+
+
 def main():
-    dispatch()
 
+    if args.get(0) in cmd_map:
 
-def dispatch():
+        arg = args.get(0)
+        args.remove(arg)
 
-    if clint.args.contains(('-h', '--help')):
+        cmd_map.get(arg).__call__(args)
+        sys.exit()
+
+    elif args.contains(('-h', '--help')):
         display_info()
         sys.exit(1)
 
-    elif clint.args.contains(('-v', '--version')):
+    elif args.contains(('-v', '--version')):
         display_version()
         sys.exit(1)
 
@@ -35,12 +42,22 @@ def dispatch():
 
 
 
+def cmd_switch(args):
+    to_branch = args.get(0)
+
+    if not to_branch:
+        print 'Avaiable branches (sorted by commit recency):'
 
 
 def display_info():
     puts('{0} by Kenneth Reitz <me@kennethreitz.com>'.format(colored.yellow('legit')))
     puts('https://github.com/kennethreitz/legit\n')
     puts('Usage: {0}'.format(colored.blue('legit <command>')))
+    puts('Commands: {0}.\n'.format(
+        eng_join(
+            [str(colored.green(c)) for c in sorted(cmd_map.keys())]
+        )
+    ))
 
 
 def display_version():
@@ -51,3 +68,11 @@ def display_version():
 
 
 
+
+cmd_map = dict(
+    switch=cmd_switch,
+    sync=cmd_switch,
+    branch=cmd_switch,
+    publish=cmd_switch,
+    unpublish=cmd_switch
+)
