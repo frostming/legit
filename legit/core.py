352ac0534504b8f7f5533f58f408ed1eeff3a873
Kenneth Reitz <me@kennethreitz.com>
2011-06-26 16:11:28 -0400

    core's dying

--- legit/core.py
+++ legit/core.py
@@ -8,9 +8,8 @@ This module provides the basic functionality of legit.
 """
 
 from clint import resources
-from pygit2 import Repository
 
-from .helpers import find_path_above
+
 
 
 
@@ -18,15 +17,6 @@ __version__ = '0.0.1'
 __author__ = 'Kenneth Reitz'
 __license__ = 'TBD'
 
-
-def get_repo(path_to_repo):
-    return Repository(path_to_repo)
-
-
-def path_to_repo():
-    """Returns the path the current repo, if there is one."""
-
-    return find_path_above('.git')
 
 
 def get_available_branches():
