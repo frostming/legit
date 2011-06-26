a1b468c1a0523466a319aed2e2b8981696a2c2ad
Kenneth Reitz <me@kennethreitz.com>
2011-06-26 09:35:54 -0400

    reverse find git repo

--- legit/core.py
+++ legit/core.py
@@ -6,19 +6,30 @@ legit.core
 
 Not much.
 """
+import os
 
 from clint import resources
-resources.init('kennethreitz', 'legit')
-resources.user.write('config.ini', "we'll get there.")
 
 
 __version__ = '0.0.1'
+
+def find_above(*names):
+    """Attempt to locate given file by searching parent dirs."""
+
+    path = '.'
+
+    while os.path.split(os.path.abspath(path))[1]:
+        for name in names:
+            joined = os.path.join(path, name)
+            if os.path.exists(joined):
+                return os.path.abspath(joined)
+        path = os.path.join('..', path)
 
 
 def path_to_repo():
     """Returns the path the current repo, if there is one."""
 
-    return None
+    return find_above('.git')
 
 
 def get_available_branches():
@@ -36,3 +47,10 @@ def get_current_branch():
 def sync_repo():
     """Git fetch. Auto branch of remotes? pull --rebase, push."""
     pass
+
+
+print path_to_repo()
+# Init Resources
+
+resources.init('kennethreitz', 'legit')
+resources.user.write('config.ini', "we'll get there.")
