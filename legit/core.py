afe5e9a0a097d6b6615e01d574b42e6c996282d6
Kenneth Reitz <me@kennethreitz.com>
2011-06-28 11:41:33 -0400

    Disable colore for @mrtazz

--- legit/core.py
+++ legit/core.py
@@ -9,10 +9,17 @@ This module provides the basic functionality of legit.
 
 # from clint import resources
 
+import os
+import clint.textui.colored
+
+
 __version__ = '0.0.9'
 __author__ = 'Kenneth Reitz'
 __license__ = 'BSD'
 
+
+if 'LEGIT_NO_COLORS' in os.environ:
+    clint.textui.colored.DISABLE_COLOR = True
 
 # resources.init('kennethreitz', 'legit')
 # resources.user.write('config.ini', "we'll get there.")
