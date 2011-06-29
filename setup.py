9769593d88eddf2293a3c63535781644a12c61ff
Kenneth Reitz <me@kennethreitz.com>
2011-06-28 21:50:11 -0400

    windows build setup.py

--- setup.py
+++ setup.py
@@ -10,18 +10,47 @@ except ImportError:
     from distutils.core import setup
 
 
+APP_NAME = 'legit'
+APP_SCRIPT = '../legit_r'
+VERSION = '0.0.9'
 
-if sys.argv[-1] == "publish":
-    os.system('python setup.py sdist upload')
-    sys.exit()
 
+# Grab requirments.
 with open('reqs.txt') as f:
     required = f.readlines()
 
 
-setup(
-    name='legit',
-    version='0.0.9',
+settings = dict()
+
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+# Build Helper.
+if sys.argv[-1] == 'build':
+    try:
+        import py2exe
+    except ImportError:
+        print 'py2exe is required to continue.'
+        sys.exit(1)
+
+    sys.argv.append('py2exe')
+
+    settings.update(
+        console=[{'script': APP_SCRIPT}],
+        zipfile = None,
+        options = {
+            'py2exe': {
+                'compressed': 1,
+                'optimize': 0,
+                'bundle_files': 1}})
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
     description='Sexy Git CLI, Inspired by GitHub for Mac.',
     long_description=open('README.rst').read(),
     author='Kenneth Reitz',
@@ -46,3 +75,7 @@ setup(
         ],
     }
 )
+
+
+
+setup(**settings)
