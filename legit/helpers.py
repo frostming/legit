3416d24303d5b972dc2dc79ae7a2d60ba69fc79f
Laurent Bachelier <laurent@bachelier.name>
2012-03-25 16:26:56 +0200

    Let git find out where the repo is
    Ensures simpler code, and identical discovery method.

--- legit/helpers.py
+++ legit/helpers.py
@@ -16,18 +16,3 @@ _platform = platform.system().lower()
 is_osx = (_platform == 'darwin')
 is_win = (_platform == 'windows')
 is_lin = (_platform == 'linux')
-
-
-def find_path_above(*names):
-    """Attempt to locate given path by searching parent dirs."""
-
-    path = '.'
-
-    while os.path.split(os.path.abspath(path))[1]:
-        for name in names:
-            joined = os.path.join(path, name)
-            if os.path.exists(joined):
-                return os.path.abspath(joined)
-        path = os.path.join('..', path)
-
-
