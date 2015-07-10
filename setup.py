1e43b25a9bc09cb7a9001ef0561909e9915d04f2
Mirth Hickford <mirth.hickford@gmail.com>
2015-07-10 23:44:17 +0100

    clarify Python 3 support

--- setup.py
+++ setup.py
@@ -61,9 +61,13 @@ settings.update(
         'Natural Language :: English',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ),
     entry_points={
         'console_scripts': [
