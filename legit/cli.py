986e7f0b11d0882d38831fcb1ba65eed6281cfed
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-31 01:25:57 +0900

    Wrap help message with 60 max width

--- legit/cli.py
+++ legit/cli.py
@@ -473,7 +473,7 @@ def help(command, to_stderr=False):
     cmd = Command.lookup(command)
     usage = cmd.usage or ''
     alias = 'alias: %s\n\n' % ', '.join(cmd.short) if cmd.short else ''
-    help = cmd.help or ''
+    help = columns([cmd.help, 60]) or ''
     help_text = '%s\n\n%s%s' % (usage, alias, help)
     if to_stderr:
         show_error(help_text)
