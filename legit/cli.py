9a38d1e9b5de85d75b2f56716c9604500c27f6b2
Graham Ullrich <graham@flyingcracker.com>
2018-03-09 02:11:43 -0700

    Improve help output, command order

--- legit/cli.py
+++ legit/cli.py
@@ -8,32 +8,40 @@ This module provides the CLI interface to legit.
 """
 
 import click
+from clint.textui import colored
+import difflib
 
 from .core import __version__
-from .scm import *
+from .scm import SCMRepo
+from .settings import settings
 
 
-pass_repo = click.make_pass_decorator(SCMRepo)
+pass_scm = click.make_pass_decorator(SCMRepo)
 
 
-@click.group()
-@click.option('--verbose', is_flag=True,
-              help='Enables verbose mode.')
+class LegitGroup(click.Group):
+    def list_commands(self, ctx):
+        commands = super(LegitGroup, self).list_commands(ctx)
+        return [cmd for cmd in sort_with_similarity(commands)]
+
+
+@click.group(cls=LegitGroup)
+# @click.option('--verbose', is_flag=True, help='Enables verbose mode.')
 @click.version_option(__version__)
 @click.pass_context
-def cli(ctx, verbose):
-    """Legit command dispatch"""
+def cli(ctx):
+    """legit : A Kenneth Reitz Project"""
     # Create a repo object and remember it as as the context object.  From
     # this point onwards other commands can refer to it by using the
     # @pass_repo decorator.
     ctx.obj = SCMRepo()
-    if ctx.obj:
-        ctx.obj.verbose = verbose
+    # if ctx.obj:
+    #     ctx.obj.verbose = verbose
 
 
-@cli.command()
+@cli.command(short_help='Switches to specified branch.')
 @click.argument('to_branch', required=False)
-@pass_repo
+@pass_scm
 def switch(scm, to_branch):
     """Switches from one branch to another, safely stashing and restoring local changes.
     """
@@ -52,10 +60,11 @@ def switch(scm, to_branch):
         status_log(scm.repo.unstash_it, 'Restoring local changes.')
 
 
-@cli.command()
+@cli.command(short_help='Synchronizes the given branch.')
 @click.argument('to_branch', required=False)
-@pass_repo
-def sync(scm, to_branch):
+@pass_scm
+@click.pass_context
+def sync(ctx, scm, to_branch):
     """Stashes unstaged changes, Fetches remote data, Performs smart
     pull+merge, Pushes local commits up, and Unstashes changes.
 
@@ -71,8 +80,9 @@ def sync(scm, to_branch):
             is_external = True
             original_branch = scm.get_current_branch_name()
         else:
-            click.echo("Branch {0} doesn't exist. Use a branch that does.".format(colored.yellow(branch)))
-            sys.exit(1)
+            click.echo("Branch {0} doesn't exist. Use a branch that does."
+                       .format(colored.yellow(branch)))
+            raise click.Abort
     else:
         # Sync current branch.
         branch = scm.get_current_branch_name()
@@ -81,7 +91,7 @@ def sync(scm, to_branch):
     if branch in scm.get_branch_names(local=False):
 
         if is_external:
-            switch(scm, branch)
+            ctx.invoke(switch, to_branch=branch)
 
         if scm.repo.is_dirty():
             status_log(scm.stash_it, 'Saving local changes.', sync=True)
@@ -93,16 +103,17 @@ def sync(scm, to_branch):
             status_log(scm.unstash_it, 'Restoring local changes.', sync=True)
 
         if is_external:
-            switch(scm, original_branch)
+            ctx.invoke(switch, to_branch=original_branch)
 
     else:
-        click.echo('Branch {0} is not published. Publish before syncing.'.format(colored.yellow(branch)))
-        sys.exit(1)
+        click.echo('Branch {0} is not published. Publish before syncing.'
+                   .format(colored.yellow(branch)))
+        raise click.Abort
 
 
-@cli.command()
+@cli.command(short_help='Publishes specified branch to the remote.')
 @click.argument('to_branch', required=False)
-@pass_repo
+@pass_scm
 def publish(scm, to_branch):
     """Pushes an unpublished branch to a remote repository."""
 
@@ -115,22 +126,25 @@ def publish(scm, to_branch):
         if to_branch is None:
             click.echo("Using current branch {0}".format(colored.yellow(branch)))
         else:
-            click.echo("Branch {0} not found, using current branch {1}".format(colored.red(to_branch), colored.yellow(branch)))
+            click.echo(
+                "Branch {0} not found, using current branch {1}"
+                .format(colored.red(to_branch), colored.yellow(branch)))
 
     branch_names = scm.get_branch_names(local=False)
 
     if branch in branch_names:
         click.echo("Branch {0} is already published. Use a branch that is not published.".format(
             colored.yellow(branch)))
-        sys.exit(1)
+        raise click.Abort
 
     status_log(scm.publish_branch, 'Publishing {0}.'.format(
         colored.yellow(branch)), branch)
 
-@cli.command()
-@click.argument('published_branch', required=False)
-@pass_repo
-def cmd_unpublish(scm, published_branch):
+
+@cli.command(short_help='Removes specified branch from the remote.')
+@click.argument('published_branch')
+@pass_scm
+def unpublish(scm, published_branch):
     """Removes a published branch from the remote repository."""
 
     scm.repo_check(require_remote=True)
@@ -146,23 +160,23 @@ def cmd_unpublish(scm, published_branch):
     if branch not in branch_names:
         click.echo("Branch {0} isn't published. Use a branch that is published.".format(
             colored.yellow(branch)))
-        sys.exit(1)
+        raise click.Abort
 
     status_log(scm.unpublish_branch, 'Unpublishing {0}.'.format(
         colored.yellow(branch)), branch)
 
 
 @cli.command()
-@pass_repo
+@pass_scm
 def branches(scm):
-    """Displays available branches."""
+    """Get a nice pretty list of branches."""
     scm.display_available_branches()
 
 
 @cli.command()
-@pass_repo
+@pass_scm
 def undo(scm):
-    """Makes last commit not exist."""
+    """Removes the last commit from history."""
     status_log(scm.undo, 'Last commit removed from history.')
 
 
@@ -183,3 +197,41 @@ def status_log(func, message, *args, **kwargs):
             if not line.startswith('#'):
                 out.append(line)
         click.secho('\n'.join(out), fg="black")
+
+
+def handle_abort(aborted, type=None):
+    click.echo('{0} {1}'.format(colored.red('Error:'), aborted.message))
+    click.echo(str(aborted.log))
+    if type == 'merge':
+        click.echo('Unfortunately, there was a merge conflict.'
+                   ' It has to be merged manually.')
+    elif type == 'unpublish':
+        click.echo(
+            '''It seems that the remote branch has been already deleted.
+            If `legit branches` still list it as published,
+            then probably the branch has been deleted at the remote by someone else.
+            You can run `git fetch --prune` to update remote information.
+            ''')
+    raise click.Abort
+
+
+settings.abort_handler = handle_abort
+
+
+def sort_with_similarity(iterable, key=None):
+    """Sort string list with similarity following original order."""
+    if key is None:
+        key = lambda x: x
+    ordered = []
+    left_iterable = dict(zip([key(elm) for elm in iterable], iterable))
+    for k in list(left_iterable.keys()):
+        if k not in left_iterable:
+            continue
+        ordered.append(left_iterable[k])
+        del left_iterable[k]
+        # find close named iterable
+        close_iterable = difflib.get_close_matches(k, left_iterable.keys())
+        for close in close_iterable:
+            ordered.append(left_iterable[close])
+            del left_iterable[close]
+    return ordered
