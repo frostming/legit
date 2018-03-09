5197206753423837245ad94f4c7277099f8f0a6b
Graham Ullrich <graham@flyingcracker.com>
2018-03-08 23:46:48 -0700

    Add sync and branches commands

--- legit/cli.py
+++ legit/cli.py
@@ -8,13 +8,12 @@ This module provides the CLI interface to legit.
 """
 
 import click
-from clint.textui import colored, columns
 
 from .core import __version__
 from .scm import *
 
 
-pass_repo = click.make_pass_decorator(Repo)
+pass_repo = click.make_pass_decorator(SCMRepo)
 
 
 @click.group()
@@ -27,42 +26,98 @@ def cli(ctx, verbose):
     # Create a repo object and remember it as as the context object.  From
     # this point onwards other commands can refer to it by using the
     # @pass_repo decorator.
-    ctx.obj = get_repo()
+    ctx.obj = SCMRepo()
     if ctx.obj:
-        ctx.obj.remote = get_remote(ctx.obj)
         ctx.obj.verbose = verbose
 
 
 @cli.command()
-@click.argument('to_branch')
+@click.argument('to_branch', required=False)
 @pass_repo
-def switch(repo, to_branch):
-    """Switches from one branch to another, safely stashing/restoring local changes.
+def switch(scm, to_branch):
+    """Switches from one branch to another, safely stashing and restoring local changes.
     """
     if to_branch is None:
         print('Please specify a branch to switch:')
-        display_available_branches(repo)
+        scm.display_available_branches()
         raise click.Abort
 
-    if repo.is_dirty():
-        status_log(stash_it, 'Saving local changes.', repo)
+    if scm.repo.is_dirty():
+        status_log(scm.stash_it, 'Saving local changes.')
 
-    status_log(checkout_branch, 'Switching to {0}.'.format(
-        colored.yellow(to_branch)), repo, to_branch)
+    status_log(scm.checkout_branch, 'Switching to {0}.'.format(
+        colored.yellow(to_branch)), to_branch)
 
-    if unstash_index(repo):
-        status_log(unstash_it, 'Restoring local changes.', repo)
+    if scm.unstash_index():
+        status_log(scm.repo.unstash_it, 'Restoring local changes.')
+
+
+@cli.command()
+@click.argument('branch', required=False)
+@pass_repo
+def sync(scm, to_branch):
+    """Stashes unstaged changes, Fetches remote data, Performs smart
+    pull+merge, Pushes local commits up, and Unstashes changes.
+
+    Defaults to current branch.
+    """
+
+    scm.repo_check(require_remote=True)
+
+    if to_branch:
+        # Optional branch specifier.
+        branch = scm.fuzzy_match_branch(to_branch)
+        if branch:
+            is_external = True
+            original_branch = scm.get_current_branch_name()
+        else:
+            print("{0} doesn't exist. Use a branch that does.".format(
+                colored.yellow(branch)))
+            sys.exit(1)
+    else:
+        # Sync current branch.
+        branch = scm.get_current_branch_name()
+        is_external = False
+
+    if branch in scm.get_branch_names(local=False):
+
+        if is_external:
+            switch(scm, branch)
+
+        if scm.repo.is_dirty():
+            status_log(scm.stash_it, 'Saving local changes.', sync=True)
+
+        status_log(scm.smart_pull, 'Pulling commits from the server.')
+        status_log(scm.push, 'Pushing commits to the server.', branch)
+
+        if scm.unstash_index(sync=True):
+            status_log(scm.unstash_it, 'Restoring local changes.', sync=True)
+
+        if is_external:
+            switch(scm, original_branch)
+
+    else:
+        print('{0} has not been published yet.'.format(
+            colored.yellow(branch)))
+        sys.exit(1)
+
+
+@cli.command()
+@pass_repo
+def branches(scm):
+    """Displays available branches."""
+    scm.display_available_branches()
 
 
 # -------
 # Helpers
 # -------
 
-def status_log(func, message, repo, *args, **kwargs):
+def status_log(func, message, *args, **kwargs):
     """Executes a callable with a header message."""
 
     click.echo(message)
-    log = func(repo, *args, **kwargs)
+    log = func(*args, **kwargs)
 
     if log:
         out = []
@@ -71,36 +126,3 @@ def status_log(func, message, repo, *args, **kwargs):
             if not line.startswith('#'):
                 out.append(line)
         click.secho('\n'.join(out), fg="black")
-
-
-def display_available_branches(repo):
-    """Displays available branches."""
-
-    if not repo.remotes:
-        remote_branches = False
-    else:
-        remote_branches = True
-    branches = get_branches(repo, local=True, remote_branches=remote_branches)
-
-    if not branches:
-        print(colored.red('No branches available'))
-        return
-
-    branch_col = len(max([b.name for b in branches], key=len)) + 1
-
-    for branch in branches:
-
-        try:
-            branch_is_selected = (branch.name == get_current_branch_name(repo))
-        except TypeError:
-            branch_is_selected = False
-
-        marker = '*' if branch_is_selected else ' '
-        color = colored.green if branch_is_selected else colored.yellow
-        pub = '(published)' if branch.is_published else '(unpublished)'
-
-        print(columns(
-            [colored.red(marker), 2],
-            [color(branch.name), branch_col],
-            [colored.black(pub), 14]
-        ))
--- legit/scm.py
+++ legit/scm.py
@@ -13,6 +13,7 @@ from collections import namedtuple
 from operator import attrgetter
 
 import clint
+from clint.textui import colored, columns
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
@@ -41,213 +42,311 @@ def abort(message, log=None, type=None):
     settings.abort_handler(a, type=type)
 
 
-def repo_check(repo, require_remote=False):
-    if repo is None:
-        print('Not a git repository.')
-        sys.exit(128)
+class SCMRepo(object):
+    repo = None
+    remote = None
+    verbose = False
 
-    # TODO: no remote fail
-    if not repo.remotes and require_remote:
-        print('No git remotes configured. Please add one.')
-        sys.exit(128)
-
-    # TODO: You're in a merge state.
-
-
-def stash_it(repo, sync=False):
-    repo_check(repo)
-    msg = 'syncing branch' if sync else 'switching branches'
-
-    return repo.git.execute([git,
-        'stash', 'save', '--include-untracked',
-        LEGIT_TEMPLATE.format(msg)])
-
-
-def unstash_index(repo, sync=False, branch=None):
-    """Returns an unstash index if one is available."""
-
-    repo_check(repo)
-
-    stash_list = repo.git.execute([git, 'stash', 'list'])
-
-    if branch is None:
-        branch = get_current_branch_name(repo)
-
-    for stash in stash_list.splitlines():
-
-        verb = 'syncing' if sync else 'switching'
-
-        if (
-            (('Legit' in stash) and
-                ('On {0}:'.format(branch) in stash) and
-                (verb in stash))
-            or (('GitHub' in stash) and
-                ('On {0}:'.format(branch) in stash) and
-                (verb in stash))
-        ):
-            return stash[7]
-
-
-def unstash_it(repo, sync=False, branch=None):
-    """Unstashes changes from current branch for branch sync."""
-
-    repo_check(repo)
-
-    stash_index = unstash_index(repo, sync=sync, branch=branch)
-
-    if stash_index is not None:
-        return repo.git.execute([git,
-            'stash', 'pop', 'stash@{{{0}}}'.format(stash_index)])
-
-
-def smart_pull(repo):
-    'git log --merges origin/master..master'
-
-    repo_check(require_remote=True)
-
-    branch = get_current_branch_name()
-
-    repo.git.execute([git, 'fetch', repo.remote.name])
-
-    return smart_merge('{0}/{1}'.format(repo.remote.name, branch), smart_merge_enabled())
-
-
-def smart_merge_enabled(repo):
-    reader = repo.config_reader()
-    if reader.has_option('legit', 'smartMerge'):
-        return reader.getboolean('legit', 'smartMerge')
-    else:
-        return True
-
-
-def smart_merge(repo, branch, allow_rebase=True):
-
-    repo_check(repo)
-
-    from_branch = get_current_branch_name()
-
-    merges = repo.git.execute([git,
-        'log', '--merges', '{0}..{1}'.format(branch, from_branch)])
-
-    if allow_rebase:
-        verb = 'merge' if merges.count('commit') else 'rebase'
-    else:
-        if git_pull_rebase():
-            verb = 'rebase'
-        else:
-            verb = 'merge'
-
-    if git_pull_ff_only():
-        return repo.git.execute([git, verb, '--ff-only', branch])
-    else:
+    def __init__(self):
         try:
-            return repo.git.execute([git, verb, branch])
-        except GitCommandError as why:
-            log = repo.git.execute([git, verb, '--abort'])
-            abort('Merge failed. Reverting.',
-                  log='{0}\n{1}'.format(why, log), type='merge')
+            self.repo = Repo(search_parent_directories=True)
+            self.remote = self.get_remote()
+        except InvalidGitRepositoryError:
+            self.repo = None
 
+    def repo_check(self, require_remote=False):
+        if self.repo is None:
+            print('Not a git repository.')
+            sys.exit(128)
 
-def git_pull_rebase(repo):
-    reader = repo.config_reader()
-    if reader.has_option('pull', 'rebase'):
-        return reader.getboolean('pull', 'rebase')
-    else:
-        return False
+        # TODO: no remote fail
+        if not self.repo.remotes and require_remote:
+            print('No git remotes configured. Please add one.')
+            sys.exit(128)
 
+        # TODO: You're in a merge state.
 
-def git_pull_ff_only(repo):
-    reader = repo.config_reader()
-    if reader.has_option('pull', 'ff'):
-        if reader.getboolean('pull', 'ff') == "only":
+    def stash_it(self, sync=False):
+        self.repo_check()
+        msg = 'syncing branch' if sync else 'switching branches'
+
+        return self.repo.git.execute(
+            [git, 'stash', 'save', '--include-untracked', LEGIT_TEMPLATE.format(msg)])
+
+    def unstash_index(self, sync=False, branch=None):
+        """Returns an unstash index if one is available."""
+
+        self.repo_check()
+
+        stash_list = self.repo.git.execute([git, 'stash', 'list'])
+
+        if branch is None:
+            branch = self.get_current_branch_name()
+
+        for stash in stash_list.splitlines():
+
+            verb = 'syncing' if sync else 'switching'
+
+            if (
+                (('Legit' in stash) and
+                    ('On {0}:'.format(branch) in stash) and
+                    (verb in stash))
+                or (('GitHub' in stash) and
+                    ('On {0}:'.format(branch) in stash) and
+                    (verb in stash))
+            ):
+                return stash[7]
+
+    def unstash_it(self, sync=False, branch=None):
+        """Unstashes changes from current branch for branch sync."""
+
+        self.repo_check()
+
+        stash_index = self.unstash_index(sync=sync, branch=branch)
+
+        if stash_index is not None:
+            return self.repo.git.execute(
+                [git, 'stash', 'pop', 'stash@{{{0}}}'.format(stash_index)])
+
+    def smart_pull(self):
+        """
+        'git log --merges origin/master..master'
+        """
+
+        self.repo_check(require_remote=True)
+
+        branch = self.get_current_branch_name()
+
+        self.repo.git.execute([git, 'fetch', self.remote.name])
+
+        return self.smart_merge('{0}/{1}'.format(self.remote.name, branch),
+                                self.smart_merge_enabled())
+
+    def smart_merge_enabled(self):
+        reader = self.repo.config_reader()
+        if reader.has_option('legit', 'smartMerge'):
+            return reader.getboolean('legit', 'smartMerge')
+        else:
             return True
+
+    def smart_merge(self, branch, allow_rebase=True):
+
+        self.repo_check()
+
+        from_branch = self.get_current_branch_name()
+
+        merges = self.repo.git.execute(
+            [git, 'log', '--merges', '{0}..{1}'.format(branch, from_branch)])
+
+        if allow_rebase:
+            verb = 'merge' if merges.count('commit') else 'rebase'
+        else:
+            if self.git_pull_rebase():
+                verb = 'rebase'
+            else:
+                verb = 'merge'
+
+        if self.git_pull_ff_only():
+            return self.repo.git.execute([git, verb, '--ff-only', branch])
+        else:
+            try:
+                return self.repo.git.execute([git, verb, branch])
+            except GitCommandError as why:
+                log = self.repo.git.execute([git, verb, '--abort'])
+                abort('Merge failed. Reverting.',
+                      log='{0}\n{1}'.format(why, log), type='merge')
+
+    def git_pull_rebase(self):
+        reader = self.repo.config_reader()
+        if reader.has_option('pull', 'rebase'):
+            return reader.getboolean('pull', 'rebase')
         else:
             return False
-    else:
-        return False
 
-
-def push(repo, branch=None):
-
-    repo_check(repo, require_remote=True)
-
-    if branch is None:
-        return repo.git.execute([git, 'push'])
-    else:
-        return repo.git.execute([git, 'push', repo.remote.name, branch])
-
-
-def checkout_branch(repo, branch):
-    """Checks out given branch."""
-
-    repo_check(repo)
-
-    _, stdout, stderr = repo.git.execute([git, 'checkout', branch],
-                                         with_extended_output=True)
-    return '\n'.join([stderr, stdout])
-
-
-def unpublish_branch(repo, branch):
-    """Unpublishes given branch."""
-
-    repo_check(repo, require_remote=True)
-
-    try:
-        return repo.git.execute([git,
-            'push', repo.remote.name, ':{0}'.format(branch)])
-    except GitCommandError:
-        _, _, log = repo.git.execute([git, 'fetch', repo.remote.name, '--prune'],
-                                     with_extended_output=True)
-        abort('Unpublish failed. Fetching.', log=log, type='unpublish')
-
-
-def publish_branch(repo, branch):
-    """Publishes given branch."""
-
-    repo_check(repo, require_remote=True)
-
-    return repo.git.execute([git,
-        'push', '-u', repo.remote.name, branch])
-
-
-def get_repo():
-    """Returns the current Repo, based on path."""
-
-    try:
-        return Repo(search_parent_directories=True)
-    except InvalidGitRepositoryError:
-        pass
-
-
-def get_remote(repo):
-
-    repo_check(repo)
-
-    reader = repo.config_reader()
-
-    # If there is no remote option in legit section, return default
-    if reader.has_option('legit', 'remote'):
-        remote_name = reader.get('legit', 'remote')
-        if remote_name not in [r.name for r in repo.remotes]:
-            if fallback_enabled(reader):
-                return get_default_remote()
+    def git_pull_ff_only(self):
+        reader = self.repo.config_reader()
+        if reader.has_option('pull', 'ff'):
+            if reader.getboolean('pull', 'ff') == "only":
+                return True
             else:
-                print('Remote "{0}" does not exist!'.format(remote_name))
-                will_aborted = clint.textui.prompt.yn(
-                    '\nPress `y` to abort now,\n' +
-                    '`n` to use default remote and turn fallback on for this repo:')
-                if will_aborted:
-                    print('\nAborted. Please update your git configuration.')
-                    sys.exit(64)  # EX_USAGE
-                else:
-                    writer = repo.config_writer()
-                    writer.set_value('legit', 'remoteFallback', 'true')
-                    print('\n`legit.RemoteFallback` changed to true for current repo.')
-                    return get_default_remote(repo)
+                return False
         else:
-            return repo.remote(remote_name)
-    else:
-        return get_default_remote(repo)
+            return False
+
+    def push(self, branch=None):
+
+        self.repo_check(require_remote=True)
+
+        if branch is None:
+            return self.repo.git.execute([git, 'push'])
+        else:
+            return self.repo.git.execute([git, 'push', self.remote.name, branch])
+
+    def checkout_branch(self, branch):
+        """Checks out given branch."""
+
+        self.repo_check()
+
+        _, stdout, stderr = self.repo.git.execute(
+            [git, 'checkout', branch],
+            with_extended_output=True)
+        return '\n'.join([stderr, stdout])
+
+    def unpublish_branch(self, branch):
+        """Unpublishes given branch."""
+
+        self.repo_check(require_remote=True)
+
+        try:
+            return self.repo.git.execute(
+                [git, 'push', self.remote.name, ':{0}'.format(branch)])
+        except GitCommandError:
+            _, _, log = self.repo.git.execute(
+                [git, 'fetch', self.remote.name, '--prune'],
+                with_extended_output=True)
+            abort('Unpublish failed. Fetching.', log=log, type='unpublish')
+
+    def publish_branch(self, branch):
+        """Publishes given branch."""
+
+        self.repo_check(require_remote=True)
+
+        return self.repo.git.execute(
+            [git, 'push', '-u', self.remote.name, branch])
+
+    def get_remote(self):
+
+        self.repo_check()
+
+        reader = self.repo.config_reader()
+
+        # If there is no remote option in legit section, return default
+        if reader.has_option('legit', 'remote'):
+            remote_name = reader.get('legit', 'remote')
+            if remote_name not in [r.name for r in self.repo.remotes]:
+                if fallback_enabled(reader):
+                    return self.get_default_remote()
+                else:
+                    print('Remote "{0}" does not exist!'.format(remote_name))
+                    will_aborted = clint.textui.prompt.yn(
+                        '\nPress `y` to abort now,\n' +
+                        '`n` to use default remote and turn fallback on for this repo:')
+                    if will_aborted:
+                        print('\nAborted. Please update your git configuration.')
+                        sys.exit(64)  # EX_USAGE
+                    else:
+                        writer = self.repo.config_writer()
+                        writer.set_value('legit', 'remoteFallback', 'true')
+                        print('\n`legit.RemoteFallback` changed to true for current repo.')
+                        return self.get_default_remote()
+            else:
+                return self.remote(remote_name)
+        else:
+            return self.get_default_remote()
+
+    def get_default_remote(self):
+        if len(self.repo.remotes) == 0:
+            return None
+        else:
+            return self.repo.remotes[0]
+
+    def get_current_branch_name(self):
+        """Returns current branch name"""
+
+        self.repo_check()
+
+        return self.repo.head.ref.name
+
+    def fuzzy_match_branch(self, branch):
+        if not branch:
+            return False
+
+        all_branches = self.get_branch_names()
+        if branch in all_branches:
+            return branch
+
+        def branch_fuzzy_match(b):
+            return b.startswith(branch)
+
+        possible_branches = list(filter(branch_fuzzy_match, all_branches))
+
+        if len(possible_branches) == 1:
+            return possible_branches[0]
+
+        return branch
+
+    def get_branches(self, local=True, remote_branches=True):
+        """Returns a list of local and remote branches."""
+
+        self.repo_check()
+
+        if not self.repo.remotes:
+            remote_branches = False
+
+        branches = []
+
+        if remote_branches:
+
+            # Remote refs.
+            try:
+                for b in self.remote.refs:
+                    name = '/'.join(b.name.split('/')[1:])
+
+                    if name not in settings.forbidden_branches:
+                        branches.append(Branch(name, is_published=True))
+            except (IndexError, AssertionError):
+                pass
+
+        if local:
+
+            # Local refs.
+            for b in [h.name for h in self.repo.heads]:
+
+                if (not remote_branches) or (b not in [br.name for br in branches]):
+                    if b not in settings.forbidden_branches:
+                        branches.append(Branch(b, is_published=False))
+
+        return sorted(branches, key=attrgetter('name'))
+
+    def get_branch_names(self, local=True, remote_branches=True):
+
+        self.repo_check()
+
+        branches = self.get_branches(local=local, remote_branches=remote_branches)
+
+        return [b.name for b in branches]
+
+    def display_available_branches(self):
+        """Displays available branches."""
+
+        if not self.repo.remotes:
+            remote_branches = False
+        else:
+            remote_branches = True
+        branches = self.get_branches(local=True, remote_branches=remote_branches)
+
+        if not branches:
+            print(colored.red('No branches available'))
+            return
+
+        branch_col = len(max([b.name for b in branches], key=len)) + 1
+
+        for branch in branches:
+
+            try:
+                branch_is_selected = (branch.name == self.get_current_branch_name())
+            except TypeError:
+                branch_is_selected = False
+
+            marker = '*' if branch_is_selected else ' '
+            color = colored.green if branch_is_selected else colored.yellow
+            pub = '(published)' if branch.is_published else '(unpublished)'
+
+            print(columns(
+                [colored.red(marker), 2],
+                [color(branch.name), branch_col],
+                [colored.black(pub), 14]
+            ))
 
 
 # Instead of getboolean('legit', 'remoteFallback', fallback=False)
@@ -257,62 +356,3 @@ def fallback_enabled(reader):
         return reader.getboolean('legit', 'remoteFallback')
     else:
         return False
-
-
-def get_default_remote(repo):
-    if len(repo.remotes) == 0:
-        return None
-    else:
-        return repo.remotes[0]
-
-
-def get_current_branch_name(repo):
-    """Returns current branch name"""
-
-    repo_check(repo)
-
-    return repo.head.ref.name
-
-
-def get_branches(repo, local=True, remote_branches=True):
-    """Returns a list of local and remote branches."""
-
-    repo_check(repo)
-
-    if not repo.remotes:
-        remote_branches = False
-
-    branches = []
-
-    if remote_branches:
-
-        # Remote refs.
-        try:
-            for b in repo.remote.refs:
-                name = '/'.join(b.name.split('/')[1:])
-
-                if name not in settings.forbidden_branches:
-                    branches.append(Branch(name, is_published=True))
-        except (IndexError, AssertionError):
-            pass
-
-    if local:
-
-        # Local refs.
-        for b in [h.name for h in repo.heads]:
-
-            if (not remote_branches) or (b not in [br.name for br in branches]):
-                if b not in settings.forbidden_branches:
-                    branches.append(Branch(b, is_published=False))
-
-
-    return sorted(branches, key=attrgetter('name'))
-
-
-def get_branch_names(repo, local=True, remote_branches=True):
-
-    repo_check(repo)
-
-    branches = get_branches(local=local, remote_branches=remote_branches)
-
-    return [b.name for b in branches]
