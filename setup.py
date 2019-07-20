549e9a4a28a5dbc4ae9e2d8eec8a8b502dd33bcf
Frost Ming <mianghong@gmail.com>
2019-07-20 09:15:46 +0800

    Add python37 CI

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from codecs import open  # To use a consistent encoding
 
 from setuptools import setup  # Always prefer setuptools over distutils
 
-APP_NAME = 'legit'
-APP_SCRIPT = './legit_r'
-VERSION = '1.0.1'
+APP_NAME = "legit"
+APP_SCRIPT = "./legit_r"
+VERSION = "1.0.1"
 
 
 # Grab requirements.
-with open('reqs.txt') as f:
+with open("reqs.txt") as f:
     required = f.readlines()
 
 
@@ -21,49 +21,46 @@ settings = dict()
 
 
 # Publish Helper.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
 
 
-if sys.argv[-1] == 'build_manpage':
-    os.system('rst2man.py README.rst > extra/man/legit.1')
+if sys.argv[-1] == "build_manpage":
+    os.system("rst2man.py README.rst > extra/man/legit.1")
     sys.exit()
 
 
 # Build Helper.
-if sys.argv[-1] == 'build':
-    os.system('pyinstaller --onefile legit_r')
+if sys.argv[-1] == "build":
+    os.system("pyinstaller --onefile legit_r")
 
 settings.update(
     name=APP_NAME,
     version=VERSION,
-    description='Git for Humans.',
-    long_description=open('README.rst').read(),
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/legit',
-    packages=['legit'],
+    description="Git for Humans.",
+    long_description=open("README.rst").read(),
+    author="Kenneth Reitz",
+    author_email="me@kennethreitz.com",
+    url="https://github.com/kennethreitz/legit",
+    packages=["legit"],
     install_requires=required,
-    license='BSD',
+    license="BSD",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    entry_points={
-        'console_scripts': [
-            'legit = legit.cli:cli',
-        ],
-    }
+    entry_points={"console_scripts": ["legit = legit.cli:cli"]},
 )
 
 
