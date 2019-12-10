07ca5c3c2819cedda8cc102e1a73c5024af9a62b
frostming <frostming@tencent.com>
2019-12-10 16:05:54 +0800

    Drop support for Python 3.4

--- setup.py
+++ setup.py
@@ -5,27 +5,53 @@ import os
 import sys
 import re
 from codecs import open  # To use a consistent encoding
+from shutil import rmtree
 
-from setuptools import setup  # Always prefer setuptools over distutils
+from setuptools import setup, Command  # Always prefer setuptools over distutils
 
 APP_NAME = "legit"
 
 with open("legit/core.py") as f:
     VERSION = re.findall(r'^__version__ *= *[\'"](.+?)[\'"]', f.read(), flags=re.M)[0]
 
-
-# Grab requirements.
-with open("reqs.txt") as f:
-    required = f.readlines()
-
-
 settings = dict()
 
 
-# Publish Helper.
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist bdist_wheel upload")
-    sys.exit()
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree('dist')
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine...')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags...')
+        os.system('git tag -a {0} -m "v{0}"'.format(VERSION))
+        os.system('git push --tags')
+
+        sys.exit()
 
 
 if sys.argv[-1] == "build_manpage":
@@ -46,8 +72,15 @@ settings.update(
     author_email="me@kennethreitz.com",
     url="https://github.com/frostming/legit",
     packages=["legit"],
-    install_requires=required,
+    install_requires=[
+        'click',
+        'clint',
+        'crayons',
+        'GitPython',
+        'six'
+    ],
     license="BSD",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -57,12 +90,12 @@ settings.update(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     entry_points={"console_scripts": ["legit = legit.cli:cli"]},
+    cmdclass={"publish": UploadCommand}
 )
 
 
