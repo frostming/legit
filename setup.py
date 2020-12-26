c6d1df0e982d71a77bfd7354d3dfafaf33f5a536
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-12-26 23:43:59 +0200

    Drop support for EOL Python 2.7

--- setup.py
+++ setup.py
@@ -80,20 +80,19 @@ settings.update(
         'six'
     ],
     license="BSD",
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+    python_requires=">=3.5",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
     ],
     entry_points={"console_scripts": ["legit = legit.cli:cli"]},
     cmdclass={"publish": UploadCommand}
