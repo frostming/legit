b67851a9591010326282fde22373b9080ee62143
Jang Rush <weakish@gmail.com>
2022-09-11 12:23:00 +0000

    workaround clint issue on recent Python versions

--- legit/core.py
+++ legit/core.py
@@ -5,9 +5,17 @@ legit.core
 This module provides the basic functionality of legit.
 """
 
+# workaround until clint#182 is merged and released
+# https://github.com/kennethreitz-archive/clint/pull/182
+import sys
+if not sys.warnoptions:
+    if sys.version_info > (3, 7):
+        import warnings
+        warnings.simplefilter("ignore", category=SyntaxWarning)
+
 from . import bootstrap
 del bootstrap
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __author__ = 'Kenneth Reitz'
 __license__ = 'BSD'
